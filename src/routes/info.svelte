e92c3e38d4cf53e8c9d3ffcdbe3e875c7cea56f3
Azmayen Murshid <azmayen.murshid@gmail.com>
2021-12-30 17:51:53 +0000

    Phone MQ check #10.

--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -285,7 +285,7 @@
         #Experiences .descriptions {
             width: 80%;
             transform: translateX(-400px);
-            font-size: 14px;
+            font-size: 15px;
         }
 
         #Projects .heading h2 {
